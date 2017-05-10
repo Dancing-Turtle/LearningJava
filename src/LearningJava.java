48dde2ba701a3d7678f85e52ea8bfc629df96737
Dancing-Turtle <alexander.martin@ccbc.edu>
2017-05-10 18:00:51 -0400

    add primary stage

--- src/LearningJava.java
+++ src/LearningJava.java
@@ -1,5 +1,15 @@
-public class LearningJava {
-    public static void main(String[] args){
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class LearningJava extends Application{
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public void start(Stage primarystage){
+        primarystage.setTitle("JavaFX Example");
+        primarystage.setWidth(1280);
+        primarystage.setHeight(720);
+        primarystage.show();
     }
 }
